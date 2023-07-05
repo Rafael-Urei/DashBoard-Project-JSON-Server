d74b09845e9468342af95a5c1c780e58e859e30d
Rafael de Souza <112446497+Rafael-Urei@users.noreply.github.com>
2023-07-05 13:31:44 -0300

    Criei um tema personalizado no MaterialUI

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="screen">Home</div>
+      <div></div>
     </>
   );
 }
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const mainTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#00ABBD",
+      light: "#A1C7E0",
+      dark: "#026E81",
+      contrastText: "#ffffff",
+    },
+  },
+});
