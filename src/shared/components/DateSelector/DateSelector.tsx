4f3abf2e7dd3ae0b864646c56138c54c4dba3857
Rafael de Souza <112446497+Rafael-Urei@users.noreply.github.com>
2023-07-20 14:04:33 -0300

    Coloquei mais um component no Layout

---
+++ src/shared/components/DateSelector/DateSelector.tsx
@@ -0,0 +1,10 @@
+import { Box, Paper } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+
+export const DateSelector = () => {
+  return (
+    <>
+      <Box component={Paper}></Box>
+    </>
+  );
+};
