6f8aa1d9320ffcc3312bf196f8417dc484726af1
Rafael de Souza <112446497+Rafael-Urei@users.noreply.github.com>
2023-08-08 12:49:59 -0300

    Adicionei a data atual

--- src/shared/components/DateSelector/DateSelector.tsx
+++ src/shared/components/DateSelector/DateSelector.tsx
@@ -1,12 +1,7 @@
 import { Box, IconButton, Paper, Typography, useTheme } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import {
-  ChevronDown,
-  ChevronLeft,
-  ChevronRight,
-  ChevronUp,
-} from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useState } from "react";
 import { useAppDateContext } from "../../contexts/DateContext";
 import { Dayjs } from "dayjs";
